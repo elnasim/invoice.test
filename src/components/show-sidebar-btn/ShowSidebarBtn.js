6fd44521d0e51af963f03979551a2ca1850aaa88
Serega KTyH <archlis21@gmail.com>
2020-08-20 16:56:40 +0300

    img fix

--- src/components/show-sidebar-btn/ShowSidebarBtn.js
+++ src/components/show-sidebar-btn/ShowSidebarBtn.js
@@ -1,14 +1,13 @@
 import React from "react";
 import { useAppContext } from "../../AppProvider";
 import "./ShowSidebarBtn.scss";
-import icon from "../../assets/img/adjust.svg";
 
 export default function ShowSidebarBtn() {
   const { changeShowsidebar } = useAppContext();
 
   return (
     <button className="show-sidebar-btn" onClick={changeShowsidebar}>
-      <img src={icon} alt="" />
+      <img src='img/adjust.svg' alt="" />
     </button>
   );
 }
